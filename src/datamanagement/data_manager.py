33f41545a260cb8ab3a70b7b19912bd21b23f67d
valefimoff11 <val.efimoff11@gmail.com>
2025-03-25 20:13:55 +0000

    initial commit

---
+++ src/datamanagement/data_manager.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def df_shift(df, target=None, lag=0):
+    if not lag and not target:
+        return df
+    new = {}
+    for c in df.columns:
+        if c == target:
+            new[c] = df[target]
+        else:
+            new[c] = df[c].shift(periods=lag)
+    return  pd.DataFrame(data=new)
