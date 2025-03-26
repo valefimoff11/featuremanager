5feb188d9676b6fd434f2a86bc63ea79060884a0
valefimoff11 <val.efimoff11@gmail.com>
2025-03-26 16:19:56 +0000

    initial commit

--- src/featurereader/rd.py
+++ src/featurereader/rd.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 import pyarrow.parquet as pq
 
-prices_df = pd.read_parquet("E:\\tst-data\\prices.parquet")
+def get_prices_df():
+    return pd.read_parquet("E:\\tst-data\\prices.parquet")
+def get_features_df():
+    return pd.read_parquet("E:\\tst-data\\features.parquet")
 
-features_df = pd.read_parquet("E:\\tst-data\\features.parquet")
-
-print(prices_df)
-print()
-print(features_df)
