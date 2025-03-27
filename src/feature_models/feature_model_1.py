4c4fd1da6f2ef1097cfd4a9f27bac77dbea690b6
valefimoff11 <val.efimoff11@gmail.com>
2025-03-27 13:46:29 +0000

    initial commit

--- src/feature_models/feature_model_1.py
+++ src/feature_models/feature_model_1.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.datamanagement.data_manager import df_shift
 from src.featurereader.rd import get_prices_df, get_features_df
 
+# for prod solution this would be populated from config file
 time_lags = [1, 5, 10]
 number_of_timelags = len(time_lags)
 cors = {}
@@ -14,6 +15,12 @@ features_df = get_features_df()
 
 print(prices_df)
 print(features_df)
+
+#################################################################################################################
+#
+# Data Preparation
+#
+#################################################################################################################
 
 feature_names = features_df.columns.values.tolist()
 feature_names.remove('timestamp')
@@ -29,22 +36,51 @@ print(feature_price_cor_df)
 model_dataset_df = pd.concat([prices_df["price"], features_df], axis=1)
 print(model_dataset_df)
 
-for lag in time_lags:
-    model_df_shifted = df_shift(model_dataset_df, 'price', lag=-1*lag)
-    print(model_df_shifted)
-    print(model_df_shifted['feature_0'].corr(model_df_shifted['price']))
 
-sys.exit()
+#################################################################################################################
+#
+# Calculation of Price Returns
+#
+#################################################################################################################
+model_dataset_df['price_return'] = model_dataset_df['price'].pct_change(1)
 
-print( df_new['feature_0'].corr(df_new['feature_1']) )
-print()
-print(features_df)
-print()
-print(df_new)
-print()
-print(df_new.corr())
-
-
-print()
+model_dataset_df = model_dataset_df.dropna()
 print(model_dataset_df)
+
+#################################################################################################################
+#
+# Calculation of Correlation of Features to Price at range of timelags
+#
+#################################################################################################################
+for lag in time_lags:
+    model_df_shifted = df_shift(model_dataset_df, 'price_return', lag=-1*lag)
+    model_df_shifted = model_df_shifted.dropna()
+    print(model_df_shifted)
+    for feature_name in feature_names:
+        feature_price_cor_df.loc[lag, feature_name] = model_df_shifted[feature_name].corr(model_df_shifted['price_return'])
+        print(model_df_shifted[feature_name].corr(model_df_shifted['price_return']))
+
+print()
+print(feature_price_cor_df)
+
+#################################################################################################################
+#
+# Calculation of Correlation Matrix of Features
+#
+#################################################################################################################
+print()
+features_only_df = features_df[features_df.columns.difference(['timestamp'])]
+features_correlation_matrix = features_only_df.corr()
+print(features_correlation_matrix)
+
+#################################################################################################################
+#
+# Calculation of Standard Deviation of Features
+#
+#################################################################################################################
+print()
+print(features_only_df.std())
+
+
+
 
