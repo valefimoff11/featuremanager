25673aebe3201aef357d171e3ecd30edb9b17d4c
valefimoff11 <val.efimoff11@gmail.com>
2025-03-27 16:34:36 +0000

    initial commit

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -7,7 +7,6 @@ class MarketResearchShell(cmd.Cmd):
     prompt = 'markets_research> '
     file = None
 
-    # ----- basic commands -----
     def do_get_feature_std(self, arg):
         'Get the standard deviation of a feature:  get_feature_std feature_0'
         print(query_key_from_db(arg))
@@ -23,9 +22,6 @@ class MarketResearchShell(cmd.Cmd):
             self.file.close()
             self.file = None
 
-def parse(arg):
-    'Convert a series of zero or more numbers to an argument tuple'
-    return tuple(map(int, arg.split()))
 
 if __name__ == '__main__':
     MarketResearchShell().cmdloop()
