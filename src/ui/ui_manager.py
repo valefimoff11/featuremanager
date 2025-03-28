6c2632d3b19d5e7752f6a926353be323871dff7d
valefimoff11 <val.efimoff11@gmail.com>
2025-03-28 12:39:26 +0000

    initial commit

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -1,30 +1,38 @@
 import cmd, sys
 
-from src.io_interfaces.io_interfaces import query_key_from_db
+from src.io_interfaces.io_interfaces import IOInterfaces
 from src.models.feature_model_1 import FeatureStatsModel
-
 
 class MarketResearchShell(cmd.Cmd):
 
     intro = 'Welcome to the Markets Research Platform shell.   Type help or ? to list commands.\n'
     prompt = 'markets_research> '
     file = None
+    io_interfaces = None
+
+    def do_get_io_paths(self, arg):
+        'Get the standard deviation of a feature:  get_io_paths'
+
+        DB_LOCATION = "E:\\tst-data\\feature_stats_db"
+        FEATURES_PATH = "E:\\tst-data\\features.parquet"
+        PRICES_PATH = "E:\\tst-data\\prices.parquet"
+        MarketResearchShell.io_interfaces = IOInterfaces(PRICES_PATH, FEATURES_PATH, DB_LOCATION)
 
     def do_get_feature_std(self, arg):
         'Get the standard deviation of a feature:  get_feature_std feature_0'
-        print(query_key_from_db(arg))
+        print(MarketResearchShell.io_interfaces.query_key_from_db(arg))
 
     def do_run_model(self, arg):
         'Run the Feature Stats Model and persist its results in key/value object DB:  run_model'
         FeatureStatsModel().run_model()
 
-    def do_close(self, arg):
+    def do_quit(self, arg):
         'Close the CLI Shell and Exit:  BYE'
         print('Thank you for using the Market Research Platform')
-        self.close()
+        self.quit()
         return True
 
-    def close(self):
+    def quit(self):
         if self.file:
             self.file.close()
             self.file = None
